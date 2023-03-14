4dc643ee23044db3cdcf63e170dbc1899f7230e8
Eli Yakubov <eli.yakubov@ness.com>
2023-03-14 15:45:12 -0400

    Clean up

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -4,6 +4,7 @@ import { select } from "../../stores/selectedImageSlice";
 import { AppDispatch, RootState } from "../../stores/store";
 import { SortBy } from "../../types/SortBy";
 import ImageCard from "../ImageCard/ImageCard";
+import "./image-gallery.css";
 
 interface Props {
   sortBy: SortBy;
@@ -14,6 +15,10 @@ const ImageGallery: React.FC<Props> = ({ sortBy }: Props) => {
   const dispatch = useDispatch<AppDispatch>();
   const favorites = images.data.filter((e) => e.favorited);
 
+  // Effect to determine which image should be selected when first mounting, and when changing between recently added and favorited
+  // During mount: Select first (most recent) image
+  // When switching from recents to favorites: If image is favorited, do nothing. Otherwise select first image in favorites
+  // When switching from favorites to recents: Do nothing
   useEffect(() => {
     if (images.status !== "success") return;
     if (sortBy === "recent" && !selectedImage.id) {
@@ -27,27 +32,29 @@ const ImageGallery: React.FC<Props> = ({ sortBy }: Props) => {
   }, [sortBy, images]);
 
   if (images.status === "fail") {
-    return <>There was an error, {images.error?.message}</>;
+    return (
+      <p>There was an error retrieving the images{images.error?.message}</p>
+    );
   }
 
   if (sortBy === "favorited") {
     return (
-      <>
+      <div className="image-gallery">
         {favorites &&
           favorites.map((data) => {
             return <ImageCard key={data.id} imageData={data} />;
           })}
-      </>
+      </div>
     );
   }
 
   return (
-    <>
+    <div className="image-gallery">
       {images.data &&
         images.data.map((data) => {
           return <ImageCard key={data.id} imageData={data} />;
         })}
-    </>
+    </div>
   );
 };
 
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,14 @@
+import Container from "../components/Container/Container";
+import Content from "../components/Content/Content";
+import Sidebar from "../components/Sidebar/Sidebar";
+
 const AppLayout: React.FC = () => {
-  return <></>;
+  return (
+    <Container>
+      <Content />
+      <Sidebar />
+    </Container>
+  );
 };
 
 export default AppLayout;
