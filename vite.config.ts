5f77b15a157d49eb6b49cf666974fad24d73313c
Eli Yakubov <eli.yakubov@ness.com>
2023-03-14 15:57:48 -0400

    Config update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/aa-challenge",
+  base: "/aa-challenge/",
   plugins: [react()],
 });
